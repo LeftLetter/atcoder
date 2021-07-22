f5de6a56aecabd7bb8d3d458fddc4b861f823102
LeftLetter <zmsh_white@yahoo.co.jp>
2021-07-22 07:05:33 +0000

    テンプレートにデバッグ設定を追加

--- pypy-7.3.0/template.py
+++ pypy-7.3.0/template.py
@@ -1,6 +1,14 @@
 import sys
+import io
+
+
 def input():
+    if "debugpy" in sys.modules:
+        input_data = """\
+        """
+        sys.stdin = io.StringIO(input_data)
     return sys.stdin.readline()[:-1]
+
 
 n = input()
 k = int(input())
